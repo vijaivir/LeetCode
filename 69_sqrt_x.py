53c938f564e1ba9e98b30b315ca70f9ad09ff120
Vijaivir Dhaliwal <91633223+vijaivir@users.noreply.github.com>
2022-08-08 12:44:19 -0700

    Update 69_sqrt_x.py

--- 69_sqrt_x.py
+++ 69_sqrt_x.py
@@ -1,3 +1,13 @@
+# Using Binary Search
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return int(sqrt(x))
+        left = 0
+        right = x
+        while left <= right:
+            mid = left + (right - left) // 2
+            if mid * mid <= x < (mid + 1) * (mid + 1):
+                return mid
+            elif x < mid * mid:
+                right = mid - 1
+            else:
+                left = mid + 1
