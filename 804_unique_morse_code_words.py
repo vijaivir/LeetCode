fd27f792a004f3df6a3bf6c2680ec4c50f43e3b5
Vijaivir Dhaliwal <91633223+vijaivir@users.noreply.github.com>
2022-08-17 13:51:31 -0700

    Update 804_unique_morse_code_words.py

--- 804_unique_morse_code_words.py
+++ 804_unique_morse_code_words.py
@@ -1,3 +1,5 @@
+# ord() returns the Unicode value and 96 is subtracted to get
+# the associated ASCII (97) to start indexing from 0 (i.e A=0, B=1 ...)
 class Solution:
     def uniqueMorseRepresentations(self, words: List[str]) -> int:
         morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
