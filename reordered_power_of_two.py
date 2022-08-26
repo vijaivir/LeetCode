7639af079571fbc18b8a1831891d0354b0cc4826
Vijaivir Dhaliwal <91633223+vijaivir@users.noreply.github.com>
2022-08-26 09:19:11 -0700

    Problem 869 solution

---
+++ reordered_power_of_two.py
@@ -0,0 +1,5 @@
+from collections import Counter
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        count = Counter(str(n))
+        return any(count == Counter(str(1 << b)) for b in range(31))
