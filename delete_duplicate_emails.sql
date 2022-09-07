88c4e3799f1216829702e9f872646c575a0a66a9
Vijaivir Dhaliwal <91633223+vijaivir@users.noreply.github.com>
2022-09-07 10:39:19 -0700

    Problem 196 solution

---
+++ delete_duplicate_emails.sql
@@ -0,0 +1,5 @@
+# Please write a DELETE statement and DO NOT write a SELECT statement.
+# Write your MySQL query statement below
+DELETE a
+FROM Person a, Person b
+WHERE a.Email = b.Email AND a.Id > b.Id
