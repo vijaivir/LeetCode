483912423b7ac36b4db38ab5c4acc22323b1a771
Vijaivir Dhaliwal <91633223+vijaivir@users.noreply.github.com>
2022-09-13 09:49:02 -0700

    Problem 35 solution
    Updated with Binary Search approach

--- search_insert_position.py
+++ search_insert_position.py
@@ -1,8 +1,10 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        if target in nums:
-            return nums.index(target)
-        else:
-            nums.append(target)
-            result = sorted(nums)
-            return result.index(target)
+        left, right = 0, len(nums)
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] >= target:
+                right = mid
+            else:
+                left = mid + 1
+        return left
