71f95261fd6ff5667c87841b07f8cf0a9f577707
Vijaivir Dhaliwal <91633223+vijaivir@users.noreply.github.com>
2022-09-14 11:48:00 -0700

    Problem 977 solution

--- squares_of_a_sorted_array.py
+++ squares_of_a_sorted_array.py
@@ -3,3 +3,19 @@ class Solution:
         for x in range(len(nums)):
             nums[x] *= nums[x]
         return sorted(nums)
+
+    
+#Binary Search Approach using two pointers (slower runtime but better solution, would be faster for large inputs)
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        answer = collections.deque()
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            left, right = abs(nums[l]), abs(nums[r])
+            if left > right:
+                answer.appendleft(left * left)
+                l += 1
+            else:
+                answer.appendleft(right * right)
+                r -= 1
+        return list(answer)
