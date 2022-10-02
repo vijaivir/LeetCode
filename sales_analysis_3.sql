6b894cf845c3db6c30cdcef4f8ea149f3a654e7e
Vijaivir Dhaliwal <91633223+vijaivir@users.noreply.github.com>
2022-10-02 11:23:07 -0700

    Problem 1084 solution

---
+++ sales_analysis_3.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT p.product_id, p.product_name
+FROM Product p
+INNER JOIN Sales s ON p.product_id = s.product_id
+GROUP BY s.product_id
+HAVING MIN(sale_date) >= '2019-01-01' 
+AND MAX(sale_date) <= '2019-03-31';
